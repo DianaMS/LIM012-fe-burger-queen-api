7b640938f8002a405a38e86b3bbc913544611cfb
DianaMS <dmartinezs@uni.pe>
2020-07-08 17:15:54 -0500

    requerir MongoLib

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
-const MongoLib = require('./lib/mongoLib');
+const MongoLib = require('./db-data/mongoLib');
 
-const { port, dbUrl, secret, dbName } = config;
+const {
+  port, dbUrl, secret, dbName,
+} = config;
 const app = express();
 
 // TODO: Conección a la BD en mogodb
 const mongoClient = new MongoLib(dbName, dbUrl);
-
-console.log(mongoClient.conection());
-console.log(dbUrl);
-console.log(dbName);
 
 app.set('config', config);
 app.set('pkg', pkg);
