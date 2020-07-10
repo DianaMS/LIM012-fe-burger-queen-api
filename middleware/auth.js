470339597e794bdd6b670ba69fda0905fed63c58
DianaMS <dmartinezs@uni.pe>
2020-07-09 23:39:54 -0500

    middleware (isAuthenticated, isAdmin)

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,3 @@
-const { ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const UsersService = require('../services/usersService');
 
@@ -18,55 +17,42 @@ module.exports = (secret) => (req, resp, next) => {
     if (err) {
       return next(403);
     }
-  
-    console.log(decodedToken);
+
     const usersService = new UsersService();
     const { userId } = decodedToken;
-
     const user = await usersService.getUser({ userId });
-    console.log(user);
+
     if (user && user.email === decodedToken.userEmail
       && user.roles.admin === decodedToken.userRol.admin) {
       req.userDecoded = decodedToken;
-      console.log('solo token');
       return next();
     }
-
     return next(401);
   });
 };
 
-
 module.exports.isAuthenticated = (req) => {
   if (req.userDecoded) {
-    console.log('siiiii se autentico');
     return true;
   }
-  console.log('no se autentico');
   return false;
 };
 
 
 module.exports.isAdmin = (req) => {
-  // TODO: decidir por la informacion del request si la usuaria es admin
   if (req.userDecoded.userRol) {
-    console.log('siiiiii es admin');
     return true;
   }
-  console.log('no  es admin ');
   return false;
 };
 
-
 module.exports.requireAuth = (req, resp, next) => {
-  console.log('aqui estamos');
   return (
     (!module.exports.isAuthenticated(req))
       ? next(401)
       : next()
   );
 };
-
 
 module.exports.requireAdmin = (req, resp, next) => (
   // eslint-disable-next-line no-nested-ternary
