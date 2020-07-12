aa9ea9e829c13a4b5fe9603a26f6ba4a23b5141e
DianaMS <dmartinezs@uni.pe>
2020-07-11 22:29:04 -0500

    conexión getProduct createProduct a MongoDB

--- services/productsService.js
+++ services/productsService.js
@@ -14,6 +14,19 @@ class ProductsService {
     const products = await this.mongoDB.getAll(this.collection, query);
     return products || [];
   }
+
+  async getProduct({ productId }) {
+    const product = await this.mongoDB.getOne(this.collection, productId);
+    return product || [];
+  }
+
+  async createProduct({ product }) {
+    const createProductId = await this.mongoDB.create(this.collection, product);
+    console.log('createProduct de productService')
+    console.log(createProductId)
+    console.log(product)
+    return createProductId;
+  }
 }
 
 module.exports = ProductsService;
