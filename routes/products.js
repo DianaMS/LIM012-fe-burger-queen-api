bddf4b8d7126023e34f15726f6a6087e529ffa20
DianaMS <dmartinezs@uni.pe>
2020-07-11 23:00:10 -0500

    exportar getProduct, postProduct a get y post

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,8 @@ const {
 
 const {
   getProducts,
+  getProduct,
+  postProduct,
 } = require('../controller/products');
 
 /** @module products */
@@ -50,8 +52,7 @@ module.exports = (app, nextMain) => {
    * @code {401} si no hay cabecera de autenticación
    * @code {404} si el producto con `productId` indicado no existe
    */
-  app.get('/products/:productId', requireAuth, (req, resp, next) => {
-  });
+  app.get('/products/:productId', requireAuth, getProduct);
 
   /**
    * @name POST /products
@@ -75,9 +76,7 @@ module.exports = (app, nextMain) => {
    * @code {403} si no es admin
    * @code {404} si el producto con `productId` indicado no existe
    */
-  app.post('/products', requireAdmin, (req, resp, next) => {
-  });
-
+  app.post('/products', requireAdmin, postProduct);
 
   /**
    * @name PUT /products
