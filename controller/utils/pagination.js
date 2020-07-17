d8639ef5d45dc7b0201bf9a4c759e156083d8ffa
DianaMS <dmartinezs@uni.pe>
2020-07-16 19:17:54 -0500

    creación función  para la paginación

---
+++ controller/utils/pagination.js
@@ -0,0 +1,16 @@
+const pagination = (url, page, limit, total) => {
+    // limit-> numero de items por pagina, page -> numero de pag solicitada por el usuario
+    const prevPage = page > 1 ? ((parseInt(page, 0)) - 1) : 1;
+    const lastPage = Math.ceil(total / limit); //numero de páginas
+    const nextPage = limit * page < total ? (parseInt(page, 0) + 1) : lastPage;
+    
+    const link = {
+        first: `${url}?limit=${limit}&page=1`,
+        prev: `${url}?limit=${limit}&page=${prevPage}`,
+        next: `${url}?limit=${limit}&page=${nextPage}`,
+        last: `${url}?limit=${limit}&page=${lastPage}`,
+    }
+    return link;
+};
+
+module.exports = { pagination };
