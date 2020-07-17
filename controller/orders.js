1ecbc6d3edbf39f2a50d8a23b714fd826929c1ba
ElinAlice <nelializ02@gmail.com>
2020-07-17 14:47:12 -0500

    Refactor: funcionalidad general de orders

--- controller/orders.js
+++ controller/orders.js
@@ -7,6 +7,30 @@ const ordersService = new OrdersService();
 const usersService = new UsersService();
 const productsService = new ProductsService();
 
+// const productDetails = async (products, next) => {
+//   const orderedProducts = [];
+//   for (let i = 0; i < products; i += 1) {
+//     const { productId } = products[i];
+//     // eslint-disable-next-line no-await-in-loop
+//     const objectProduct = await productsService.getProduct({ productId });
+//     if (objectProduct === null) {
+//       return next;
+//     }
+//     orderedProducts.push(objectProduct);
+//   }
+
+//   const productsAndQuantity = orderedProducts.map((product) => {
+//     const productFilter = products
+//       .filter((element) => element.productId === product._id.toString());
+//     return {
+//       product,
+//       qty: productFilter[0].qty,
+//     };
+//   });
+
+//   return productsAndQuantity;
+// };
+
 module.exports = {
   getOrders: async (req, resp, next) => {
     const { tags } = req.query;
@@ -112,11 +136,13 @@ module.exports = {
   postOrder: async (req, resp, next) => {
     const { body: order } = req;
     const { userId } = order;
+    console.log(order);
     const productsArray = order.products;
     const orderedProducts = [];
 
     try {
       const objectUserId = await usersService.getUser({ userId });
+      console.log(objectUserId);
 
       if (!objectUserId || objectUserId === null || productsArray.length <= 0) {
         return next(400);
@@ -236,6 +262,9 @@ module.exports = {
       }
 
       const productsArray = orderObject.products;
+
+      // const productDetailsAndQuantity = await productDetails(productsArray, next(400));
+      // console.log(productDetailsAndQuantity);
       const orderedProducts = [];
 
       for (let i = 0; i < productsArray.length; i += 1) {
