a303dfd491b6c91af08cdec1659efb2b4cd3fedd
DianaMS <dmartinezs@uni.pe>
2020-07-29 02:20:43 -0500

    añadiendo jest-mongodb

--- e2e/globalSetup.js
+++ e2e/globalSetup.js
@@ -2,6 +2,10 @@ const path = require('path');
 const { spawn } = require('child_process');
 const nodeFetch = require('node-fetch');
 const kill = require('tree-kill');
+const mongoSetup = require('@shelf/jest-mongodb/setup');
+const MongodbMemoryServer = require('mongodb-memory-server').default;
+
+global.__MONGOD__ = MongodbMemoryServer;
 
 const config = require('../config');
 
@@ -98,7 +102,7 @@ const waitForServerToBeReady = (retries = 10) => new Promise((resolve, reject) =
           : resolve()
       ))
       .catch(() => waitForServerToBeReady(retries - 1).then(resolve, reject));
-  }, 200);
+  }, 1000);
 });
 
 
@@ -109,41 +113,42 @@ module.exports = () => new Promise((resolve, reject) => {
   }
 
   // TODO: Configurar DB de tests
-
-  console.info('Staring local server...');
-  const child = spawn('npm', ['start', process.env.PORT || 8888], {
-    shell: process.platform === 'win32',
-    cwd: path.resolve(__dirname, '../'),
-    stdio: ['ignore', 'pipe', 'pipe'],
-  });
-
-  Object.assign(__e2e, { childProcessPid: child.pid });
-
-  child.stdout.on('data', (chunk) => {
-    console.info(`\x1b[34m${chunk.toString()}\x1b[0m`);
-  });
-
-  child.stderr.on('data', (chunk) => {
-    const str = chunk.toString();
-    if (/DeprecationWarning/.test(str)) {
-      return;
-    }
-    console.error('child::stderr', str);
-  });
-
-  process.on('uncaughtException', (err) => {
-    console.error('UncaughtException!');
-    console.error(err);
-    kill(child.pid, 'SIGKILL', () => process.exit(1));
-  });
-
-  waitForServerToBeReady()
-    .then(checkAdminCredentials)
-    .then(createTestUser)
-    .then(resolve)
-    .catch((err) => {
-      kill(child.pid, 'SIGKILL', () => reject(err));
+  mongoSetup().then(() => {
+    console.info('Staring local server...');
+    const child = spawn('npm', ['start', process.env.PORT || 8888], {
+      shell: process.platform === 'win32',
+      cwd: path.resolve(__dirname, '../'),
+      stdio: ['ignore', 'pipe', 'pipe'],
     });
+
+    Object.assign(__e2e, { childProcessPid: child.pid });
+
+    child.stdout.on('data', (chunk) => {
+      console.info(`\x1b[34m${chunk.toString()}\x1b[0m`);
+    });
+
+    child.stderr.on('data', (chunk) => {
+      const str = chunk.toString();
+      if (/DeprecationWarning/.test(str)) {
+        return;
+      }
+      console.error('child::stderr', str);
+    });
+
+    process.on('uncaughtException', (err) => {
+      console.error('UncaughtException!');
+      console.error(err);
+      kill(child.pid, 'SIGKILL', () => process.exit(1));
+    });
+
+    waitForServerToBeReady()
+      .then(checkAdminCredentials)
+      .then(createTestUser)
+      .then(resolve)
+      .catch((err) => {
+        kill(child.pid, 'SIGKILL', () => reject(err));
+      });
+  });
 });
 
 // Export globals - ugly... :-(
