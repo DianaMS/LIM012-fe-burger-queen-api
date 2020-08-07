c64c33b5c58141c033d849d8876e5dc9e7f79384
ElinAlice <nelializ02@gmail.com>
2020-08-06 20:38:42 -0500

    Refactor: test docker

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 exports.port = process.argv[2] || process.env.PORT || 8080;
-console.log(process.env.DB_URL)
 exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://localhost:27017/test';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.MONGO_INITDB_ROOT_USERNAME || process.env.ADMIN_EMAIL || 'admin@localhost.com';
